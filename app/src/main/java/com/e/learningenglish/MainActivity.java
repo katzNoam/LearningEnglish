6b262114143bc79ca3195e86caff565400ea168b
User <uriel.rachamim@gmail.com>
2021-11-10 18:43:29 +0200

    start check 14

--- app/src/main/java/com/e/learningenglish/MainActivity.java
+++ app/src/main/java/com/e/learningenglish/MainActivity.java
@@ -1,37 +1,34 @@
 package com.e.learningenglish;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-Button btn;
-Dialog loginDialog;
+Button register;
+Dialog register_dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn=findViewById(R.id.button);
-        btn.setOnClickListener(this);
+        register=findViewById(R.id.button);
+        register.setOnClickListener(this);
     }
 
 
 
     @Override
     public void onClick(View v) {
-        if (btn==v)
+        if (register==v)
         {
-            loginDialog=new Dialog(this);
-            loginDialog.setContentView(R.layout.dialog_register);
-            loginDialog.show();
+            register_dialog =new Dialog(this);
+            register_dialog.setContentView(R.layout.dialog_register);
+            register_dialog.show();
         }
     }
 }
