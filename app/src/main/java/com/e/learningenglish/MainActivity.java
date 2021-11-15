9746ad6509c67f2dd2ee5905adcab02b72b817bb
User <uriel.rachamim@gmail.com>
2021-11-15 13:57:02 +0200

    40

--- app/src/main/java/com/e/learningenglish/MainActivity.java
+++ app/src/main/java/com/e/learningenglish/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -14,12 +15,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 Button register;
 Dialog register_dialog;
 Button connect;
+EditText name;
+EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name=findViewById(R.id.name);
+        password=findViewById(R.id.password);
         register=findViewById(R.id.button);
         register.setOnClickListener(this);
         connect=(Button)findViewById(R.id.button0);
@@ -40,6 +45,7 @@ Button connect;
         if (connect==v)
         {
             Intent intent=new Intent(this,OpeningActivity.class);
+            intent.putExtra("hello2",name.getText().toString());
             startActivity(intent);
         }
     }
--- app/src/main/java/com/e/learningenglish/OpeningActivity.java
+++ app/src/main/java/com/e/learningenglish/OpeningActivity.java
@@ -1,15 +1,18 @@
 package com.e.learningenglish;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OpeningActivity extends AppCompatActivity implements View.OnClickListener
 {
+    TextView hello;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main,menu);
@@ -22,7 +25,10 @@ public class OpeningActivity extends AppCompatActivity implements View.OnClickLi
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening);
-
+        hello=findViewById(R.id.hello);
+        Intent intent=getIntent();
+        String hello2 = intent.getExtras().getString("hello2");
+        hello.setText("Hello "+hello2+"!");
     }
 
     @Override
