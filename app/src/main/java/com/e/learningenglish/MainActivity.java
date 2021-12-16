703fb9ae2c63b7b18e0eb7497fe36635d0c8d26f
User <uriel.rachamim@gmail.com>
2021-12-16 11:55:16 +0200

    44

--- app/src/main/java/com/e/learningenglish/MainActivity.java
+++ app/src/main/java/com/e/learningenglish/MainActivity.java
@@ -8,44 +8,83 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-Button register;
-Dialog register_dialog;
-Button log_in;
-EditText name;
-EditText password;
+    Button register;
+    Dialog register_dialog;
+    Button log_in;
+    EditText name;
+    EditText password;
+
+    EditText register_name;
+    EditText register_email;
+    EditText register_password;
+    EditText register_confirm_password;
+    Button register_action;
+    FirebaseAuth auth;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name=findViewById(R.id.name);
-        password=findViewById(R.id.password);
-        register=findViewById(R.id.register);
+        name = findViewById(R.id.name);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
         register.setOnClickListener(this);
-        log_in=(Button)findViewById(R.id.log_in);
+        log_in = findViewById(R.id.log_in);
         log_in.setOnClickListener(this);
     }
 
 
+    public void createRegisterDialog() {
+        register_dialog = new Dialog(this);
+        register_dialog.setContentView(R.layout.dialog_register);
+        register_name = register_dialog.findViewById(R.id.register_name);
+        register_email = register_dialog.findViewById(R.id.register_email);
+        register_password = register_dialog.findViewById(R.id.register_password1);
+        register_confirm_password = register_dialog.findViewById(R.id.register_password2);
+        register_action = register_dialog.findViewById(R.id.register2);
+        register_action.setOnClickListener(this);
+        auth = FirebaseAuth.getInstance();
+        register_dialog.show();
+    }
 
     @Override
-    public void onClick(View v)
-    {
-        if (register==v)
-        {
-            register_dialog =new Dialog(this);
-            register_dialog.setContentView(R.layout.dialog_register);
-            register_dialog.show();
-        }
-        if (log_in ==v)
-        {
-            Intent intent=new Intent(this,OpeningActivity.class);
-            intent.putExtra("hello2",name.getText().toString());
+    public void onClick(View v) {
+        if (register == v) {
+            createRegisterDialog();
+        } else if (log_in == v) {
+            Intent intent = new Intent(this, OpeningActivity.class);
+            intent.putExtra("hello2", name.getText().toString());
             startActivity(intent);
+        } else if (v == register_action) {
+            if(register_password.getText().toString().equals("")|| register_email.getText().toString().equals("") || register_name.getText().toString().equals("") || register_confirm_password.getText().toString().equals("") ){
+                Toast.makeText(MainActivity.this, "Please fill all the form.", Toast.LENGTH_SHORT).show();
+            } else if (!register_password.getText().toString().equals(register_confirm_password.getText().toString())) {
+                Toast.makeText(this, "problem at your password", Toast.LENGTH_LONG).show();
+            } else {
+
+                auth.createUserWithEmailAndPassword(register_email.getText().toString(), register_password.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Successfully registered", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Registration Error", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+            }
         }
     }
 }
--- app/src/main/java/com/e/learningenglish/RegisterDialog.java
+++ app/src/main/java/com/e/learningenglish/RegisterDialog.java
@@ -1,14 +1,22 @@
 package com.e.learningenglish;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterDialog extends AppCompatActivity implements View.OnClickListener
 {
@@ -19,27 +27,43 @@ public class RegisterDialog extends AppCompatActivity implements View.OnClickLis
     Button register;
     FirebaseAuth auth;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_register);
-        name=findViewById(R.id.register_name);
-        email=findViewById(R.id.register_email);
-        password=findViewById(R.id.register_password1);
-        confirm_password=findViewById(R.id.register_password2);
-        register=findViewById(R.id.register2);
-        register.setOnClickListener(this);
-        auth=FirebaseAuth.getInstance();
+    }
+
+
+
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+//        updateUI(currentUser);
     }
 
     @Override
     public void onClick(View v)
     {
-        if(register==v)
+        if(v == register)
         {
-            if(!password.equals(confirm_password))
+            if(!password.getText().toString().equals(confirm_password.getText().toString()))
             {
-                Toast.makeText(this,"problem at your password",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "problem at your password",Toast.LENGTH_LONG).show();
+            } else {
+                auth.createUserWithEmailAndPassword(email.getText() + "", password.getText() + "")
+                        .addOnCompleteListener(RegisterDialog.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterDialog.this, "Successfully registered", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(RegisterDialog.this, "Registration Error", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
         }
     }
